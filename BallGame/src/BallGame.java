e52f0b4602d6db01901408292b263838d80f349c
Minifullc <1414273510@qq.com>
2018-07-18 14:11:54 +0800

    桌球小游戏项目

---
+++ BallGame/src/BallGame.java
@@ -0,0 +1,61 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class BallGame extends JFrame{
+	
+	Image ball = Toolkit.getDefaultToolkit().getImage("images/ball.png");
+	Image desk = Toolkit.getDefaultToolkit().getImage("images.desk.png");
+	
+	double x=100;
+	double y=100;
+	double degree = 3.14/3; //弧度。 60°
+	
+	
+	//画窗口的方法
+	public void paint(Graphics g) {
+		System.out.println("窗口被画了一次");
+		g.drawImage(desk, 0, 0, null);
+		g.drawImage(ball, (int)x, (int)y, null);
+
+		
+		
+		x = x+ 10*Math.cos(degree);
+		y = y+ 10*Math.sin(degree);
+		
+		if(y>500-40-30 || y<40+40) {   //40标题栏的高度
+			degree = -degree;
+		}
+
+		//左右边界
+		if(x<40 || x>856-40-30) {
+			degree = 3.14-degree;
+		}
+		
+		
+	}
+	
+	//窗口加载
+	void launchFrame() {
+		setSize(856,500);
+		setLocation(50, 50);
+		setVisible(true);
+		
+		// 重画窗口
+		while(true) {
+			repaint();
+			try {
+				Thread.sleep(40);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+	//main是程序执行的入口
+	public static void main(String[] args){
+		BallGame game = new BallGame();
+		game.launchFrame();
+	}
+	
+}
