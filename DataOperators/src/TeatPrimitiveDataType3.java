be146829b1161c63734aad6b9ea5d572e8abdd82
Minifullc <1414273510@qq.com>
2018-07-24 17:25:17 +0800

    测试字符类型

---
+++ DataOperators/src/TeatPrimitiveDataType3.java
@@ -0,0 +1,21 @@
+/**
+ * 测试字符类型
+ * @author Musuc
+ *
+ */
+public class TeatPrimitiveDataType3 {
+	public static void main(String[] args) {
+		char a = 'T';
+		char b = '我';
+		char c = '\u0061';
+		System.out.println("" + 'a' + '\n' + 'b');
+		System.out.println("" + 'a' + '\t' + 'b');
+		System.out.println("" + 'a' + '\'' + 'b');
+		
+		
+		//String就是字符序列
+		String d = "abc";
+		
+	}
+
+}
