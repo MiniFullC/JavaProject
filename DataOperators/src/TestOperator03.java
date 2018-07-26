af747782f032c4feff739919daeb7bea187c8042
Minifullc <1414273510@qq.com>
2018-07-26 17:22:10 +0800

    字符串运算符

---
+++ DataOperators/src/TestOperator03.java
@@ -0,0 +1,19 @@
+/**
+ * 字符串运算符
+ * 
+ * @author Musuc
+ *
+ */
+public class TestOperator03 {
+	public static void main(String[] args) {
+		String a = "3";
+		int b = 4;
+		int c = 5;
+		char d = 'a';
+		System.out.println(a+b);
+		System.out.println(a+c);
+		System.out.println(b+c+a);
+		System.out.println(d+4);    //d=97
+		}
+
+}
