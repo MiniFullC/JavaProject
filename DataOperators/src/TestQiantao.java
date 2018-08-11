4c92ef2add0427574da35542de6a714e3da619af
Minifullc <1414273510@qq.com>
2018-08-11 22:24:28 +0800

    测试嵌套循环

---
+++ DataOperators/src/TestQiantao.java
@@ -0,0 +1,17 @@
+/**
+ * 测试嵌套循环
+ * @author Musuc
+ *
+ */
+public class TestQiantao {
+	public static void main(String[] args) {
+	//	int i =1;
+		
+		for (int i =1;i<=5;i++) {
+			for(int j =1;j<=5;j++) {
+				System.out.print(i+"\t");
+			}
+			System.out.println();
+		}
+	}
+}
