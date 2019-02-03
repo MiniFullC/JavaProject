6d6cc40fcc617aeda59ed9ed3015a7734a207f5f
MinifullC <1414273510@qq.com>
2019-02-03 19:13:45 +0800

    IDCard

---
+++ DataOperators/src/GenIdCard.java
@@ -0,0 +1,52 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class GenIdCard {
+    static String lastIdCard17 ="37029919810801000"; //37029919820801000
+    public static String genIdCard() {
+        String lastIdCardLastThreeLetter = lastIdCard17.substring(14,17);
+        if (lastIdCardLastThreeLetter.equals("999")) {
+            String dateString = lastIdCard17.substring(6, 14);
+            Date date = null;
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+            try {
+                date = simpleDateFormat.parse(dateString);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            Calendar dateCalendar = Calendar.getInstance();
+            dateCalendar.setTime(date);
+            dateCalendar.add(Calendar.DATE, 1);
+            date = dateCalendar.getTime();
+            String dateResult = simpleDateFormat.format(date);
+            lastIdCard17 = "370299" + dateResult + "001";
+        } else {
+            long lastIdCardL = Long.valueOf(lastIdCard17.substring(0,17));
+            lastIdCardL++;
+            lastIdCard17 = String.valueOf(lastIdCardL);
+        }
+        StringBuffer idCard18 = new StringBuffer(lastIdCard17);
+        //加权因子
+        //校验码值
+        char[] checkBit = {'1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'};
+        int sum = 0;
+        for (int index = 0; index < idCard18.length(); index++) {
+            char c = idCard18.charAt(index);
+            int ai = Integer.parseInt(new Character(c).toString());
+            //加权因子的算法
+            int Wi = ((int) Math.pow(2, idCard18.length() - index)) % 11;
+            sum = sum + ai * Wi;
+        }
+        int indexOfCheckBit = sum % 11; //取模
+        idCard18.append(checkBit[indexOfCheckBit]);
+        return idCard18.toString();
+    }
+
+    public static void main(String[] args) {
+        for(int i=0;i<650;i++){
+            System.out.println(genIdCard());
+        }
+    }
+}
