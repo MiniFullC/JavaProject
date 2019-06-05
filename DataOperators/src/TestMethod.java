03d25cd557140bcebf18a76e4c50d565a5b4b305
MinifullC <1414273510@qq.com>
2019-06-05 17:27:38 +0800

    Update set over old

--- DataOperators/src/TestMethod.java
+++ DataOperators/src/TestMethod.java
@@ -1,28 +1,29 @@
+import java.awt.print.Printable;
+
 /**
  * 测试方法的基本使用
+ * 
  * @author Musuc
  *
  */
 public class TestMethod {
 	public static void main(String[] args) {
-		//通过对象调用普通方法
-		TestMethod an= new TestMethod();
-		an.printMes();
-		System.out.println("And I want to say:");
-		int c = an.add(500,20,1)+1000;
+		// 通过对象调用方法
+		TestMethod an = new TestMethod();
+		System.out.println("I want to say:");
+		an.printSxt();
+		int c = an.add(500, 20, 1) + 1000;
 		System.out.println(c);
-		
-	}
-	void printMes() {
-		System.out.println("Aln早上好");
-		System.out.println("Aln中午好");
-		System.out.println("Aln晚上好");
-	}
-	
-	int add(int a, int b, int c) {
-		int sum = a+b+c;
-		System.out.println(sum);
-		return sum;         //1.结束方法的运行 2.返回值
+
 	}
 
+	void printSxt() {
+		System.out.println("Aln 晚上好");
+	}
+
+	int add(int a, int b, int c) {
+		int sum = a + b + c;
+		System.out.println(sum);
+		return sum;
+	}
 }
